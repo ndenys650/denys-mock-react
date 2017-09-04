1c84a42f8659eda73ea65abb37c8a3443db2311f
Nate Denys <nate_denys@yahoo.com>
2017-09-04 15:50:10 -0700

    Create Footer and Add responsive submenu

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import './Footer.css'
 
 const Footer = () => {
 
     return (
       <div className="App">
-      MY FOOTER
+      	<footer className="footer is-primary">
+      <div className="container">
+        <div className="columns">
+          <div className=" column">
+            <p>If you have any questions, feel free to reach out!</p>
+          </div>
+          <div className="column has-text-right">
+            <a className="icon" href="#"><i className="fa fa-facebook"></i></a>
+            <a className="icon" href="#"><i className="fa fa-twitter"></i></a>
+          </div>
+        </div>
+      </div>
+    </footer>
       </div>
     );
 };
